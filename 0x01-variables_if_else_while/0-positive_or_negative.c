07c480cc24e0c1e62fe90e79824068e52fccdd11
Kanyunyuzi <sunikan25@gmail.com>
2022-09-09 07:41:01 -0700

    generating random numbers

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -15,16 +15,24 @@ int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 
- if (n == 0)
-   printf("%i is zero\n", n);
- else if (n < 0)
-   printf("%i is negative\n", n);
-
- else
-   printf("%i is positive\n", n);
-
- return (0);
+if (n == 0)
+{
+printf("%i is zero\n", n);
 }
+
+else if (n < 0)
+{
+printf("%i is negative\n", n);
+}
+
+else
+{
+printf("%i is positive\n", n);
+}
+
+return (0);
+
+}  
 
 	  
 
