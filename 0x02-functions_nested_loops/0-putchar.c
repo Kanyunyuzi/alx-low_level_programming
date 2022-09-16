ef0ba35eb0f4a93be34aecd960ad4bfaa1670520
Kanyunyuzi <sunikan25@gmail.com>
2022-09-16 07:13:12 -0400

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,16 +1,14 @@
-#include <stdio.h>
-#include <main.h>
-/* A program that prints putchar
- * Main - Entry point
- * Return: Always 0 (Success)
-*/
+#include "main.h"
 
-main(void)
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+int _putchar(char c)
 {
-  char a="_putchar";
-  printf("%s", a);
-  printf("\n");
- 
-  return (0);
-
-} 
+	return (write(1, &c, 1));
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,12 +1,14 @@
 #ifndef PUTCHAR_H
 #define PUTCHAR_H
 #include <stdio.h>
-/*
 
+
+/*
  *File: main.h
  *Author: Kanyunyuzi Sunitah
  *Desc: Header file containing all delarations for all function
  */
 
 int _putchar(char c);
+#endif
 
