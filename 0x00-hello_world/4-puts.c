7932730e15c2413f8f4eb5fddb33a768f53ffe97
Kanyunyuzi <sunikan25@gmail.com>
2022-09-16 00:36:17 -0700

    Using puts to print

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ *AUTHOR-KANYUNYUZI SUNITAH
+ *DATE - 16th September
+ *
+ * Using puts to print
+ *
+ *Return (0) Success
+ */
+
+int main (void)
+{
+  
+char *s = "Programming is like building a multilingual puzzle";
+puts(s);
+ 
+return (0);
+ 
+}
