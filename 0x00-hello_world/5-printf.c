400a94770a8b3c6b4966d7a187529dd5f5d694c0
Kanyunyuzi <sunikan25@gmail.com>
2022-09-16 01:53:13 -0700

    printing strings with printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/* main - Entry point 
+ *
+ *Return 0 (sucess)
+
+ */
+
+int main (void)
+{
+  char c[] = "with proper grammar, but the outcome is a piece of art,";
+    printf("%s\n", c);
+  
+
+    return (0);
+}    
+  
+  
