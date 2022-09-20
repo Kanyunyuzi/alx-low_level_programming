5c58815b3954ad39684874f55048d05620e561bb
Kanyunyuzi <sunikan25@gmail.com>
2022-09-19 22:09:19 -0700

    A function that takes a pointer to an int as a parameter

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -7,24 +7,9 @@
  *A function that takes a pointer to an int as parameter and updates the value  and points to to 98.
  * Return: Always (0)
  */
-int main(void)
-{
-  int n;
-  n = 402;
-}
- 
-/**main - parameters are passed by value
-  *
-  *Return: ALways 0.
-  */
 
-void reset_to_98 (int *n)
+void reset_to_98(int *n)
 {
-   n = &n;
-   reset_to_98(n)
    *n = 98;
-   printf("%p\n", n);
-   printf("%d\n",n):
-   return(0);
 }
 
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,15 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
-
-#include <stdio.h>
-
+int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
-
-
-/**
- *Author - Kanyunyuzi  Sunitah
- *
- *file name : main.h
- */
-#endif /* MAIN_H */
+#endif
+/** MAIN_H */
