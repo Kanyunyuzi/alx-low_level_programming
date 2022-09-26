513de4bbc8595a4876d760ccdf41650bd18c61bc
Kanyunyuzi <sunikan25@gmail.com>
2022-09-26 09:06:59 -0400

    submit

--- 0x07-pointers_arrays_strings/0-main.c
+++ 0x07-pointers_arrays_strings/0-main.c
@@ -10,33 +10,33 @@
  */
 void simple_print_buffer(char *buffer, unsigned int size)
 {
-  unsigned int i;
+        unsigned int i;
 
-  i=0;
-  while (i < size)
-    {
-      if (i % 10)
-	{
-	  printf ("");
-	}
-      if (!(i % 10) && i)
-	{
-	  printf ("\n");
-        }
-      
-         printf("0x%02x", buffer[i]);
+        i = 0;
+        while (i < size)
+        {
+                if (i % 10)
+                {
+                        printf(" ");
+                }
+                if (!(i % 10) && i)
+                {
+                        printf("\n");
+                }
+                printf("0x%02x", buffer[i]);
                 i++;
         }
         printf("\n");
 }
 
 /**
- * main - check the code
+ * main - check the code for Holberton School students.
  *
  * Return: Always 0.
  */
 int main(void)
-{  char buffer[98] = {0x00};
+{
+    char buffer[98] = {0x00};
 
     simple_print_buffer(buffer, 98);
     _memset(buffer, 0x01, 95);
